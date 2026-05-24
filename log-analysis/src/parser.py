a1fddc05a3e09ef4f7d10163cb639b43ba1d13ce
s.mosayebzadeh <m.mosaiebzadeh@gmail.com>
2026-05-24 14:50:45 +0330

    improve log parsing performance

--- log-analysis/src/parser.py
+++ log-analysis/src/parser.py
@@ -1,32 +1,24 @@
 import re
 import pandas as pd
+from datetime import datetime
 
 def parse_to_csv(filename):
 
-    date = []
-    time = []
-    date_time = []
-    level = []
-    message = []
+    raws = []
 
     with open(filename, "r") as f:
         reg = re.compile(r"^(\d{4}-\d{2}-\d{2})\s(\d{2}:\d{2}:\d{2})\s(ERROR|WARN|INFO)\s(.+)$")
-        for l in f.readlines():
-            a = re.match(reg, l)
-            
-            if a:
-                date.append(a.group(1))
-                time.append(a.group(2))
-                date_time.append(pd.to_datetime(a.group(1) + " " + a.group(2)))
-                level.append(a.group(3))
-                message.append(a.group(4))
-    
-    df = pd.DataFrame()
 
-    return df.from_dict({
-        "datetime":date_time,
-        "date":date,
-        "time":time,
-        "level":level,
-        "message":message
-    })
+        for l in f:
+            match = reg.match(l)
+            
+            if match:
+                raws.append({
+                    "datetime":datetime.strptime((match.group(1) + " " + match.group(2)), "%Y-%m-%d %H:%M:%S"),
+                    "date":match.group(1),
+                    "time":match.group(2),
+                    "level":match.group(3),
+                    "message":match.group(4)
+                })
+
+    return pd.DataFrame(raws)
